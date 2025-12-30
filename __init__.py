81f26f20e2aa5b4ce8c279a13a8c1c39c7d5bbff
Attawat.T <attawatt@xspringas.com>
2025-12-30 13:38:42 +0700

    chore: add root package initialization

---
+++ __init__.py
@@ -0,0 +1,14 @@
+"""
+ETL Core
+~~~~~~~~
+
+Core utilities and constants for the ETL pipeline.
+"""
+
+
+from . import constants
+
+__author__ = "Data Team"
+__version__ = "0.1.0"
+
+__all__ = ["constants"]
