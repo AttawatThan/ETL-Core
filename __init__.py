618868340ee8d1f5e119c0266c073ed9584f7c0e
Attawat.T <attawatt@xspringas.com>
2025-12-30 15:18:14 +0700

    fix: restore author name and remove commented import for constants

--- __init__.py
+++ __init__.py
@@ -6,9 +6,5 @@ Core utilities and constants for the ETL pipeline.
 """
 
 
-# from . import constants
-
-__author__ = "Data Team"
+__author__ = "MakaGunz"
 __version__ = "0.1.0"
-
-# __all__ = ["constants"]
